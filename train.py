40e7b8ccdb52818de1a66b36c8aebb543ced0d4d
Ubuntu <ubuntu@ip-172-31-23-175.ec2.internal>
2020-01-02 15:37:15 +0000

    argparse update

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,12 +16,31 @@ from torch.optim import lr_scheduler
 from torch.utils.data import Dataset, DataLoader
 from torchvision import models
 from torchvision import transforms, utils
-import gc
 
 import utils
 import dataset 
 import pretreat
 import model
+
+
+parser = argparse.ArgumentParser(description='training settings')
+parser.add_argument('--path', default='/apl/kaggle/baidu_drive/data/') 
+parser.add_argument('--epoch', default=10)
+parser.add_argument('--batch', default=1)
+parser.add_argument('--switch', default=0, help='switch loss epoxh')
+parser.add_argument('--model_path', default='model/resnet152_weight025/')
+parser.add_argument('--history_path', default='history/resnet/resnet152_weight025.csv')
+args = parser.parse_args()
+
+print(args)
+latest_model_path =args.model_path + 'latest.pth'
+best_model_path = args.model_path + 'best.pth'
+print(best_model_path)
+
+camera_matrix = np.array([[2304.5479, 0,  1686.2379],
+                          [0, 2305.8757, 1354.9849],
+                          [0, 0, 1]], dtype=np.float32)
+camera_matrix_inv = np.linalg.inv(camera_matrix)
 
 def criterion(prediction, mask, regr,weight=0.4, size_average=True):
     # Binary mask loss
@@ -50,10 +70,10 @@ def train(epoch, history=None):
         
         optimizer.zero_grad()
         output = model(img_batch)
-        if epoch < SWITCH_LOSS_EPOCH :
+        if epoch < args.switch :
             loss,mask_loss, regr_loss = criterion(output, mask_batch, regr_batch,1)
         else:
-            loss,mask_loss, regr_loss = criterion(output, mask_batch, regr_batch,0.5)  
+            loss,mask_loss, regr_loss = criterion(output, mask_batch, regr_batch,0.25)  
         
         if iteration % 10 == 0:
             print('epoch:{} iteration:{} loss:{:.3f} mask_loss:{:.3f} regr_loss:{:.3f}'.format(epoch+1, iteration+1, loss.data, mask_loss.data, regr_loss.data))
@@ -89,13 +109,13 @@ def evaluate(epoch, history=None):
 
             output = model(img_batch)
 
-            if epoch < SWITCH_LOSS_EPOCH :
+            if epoch < args.switch :
                 loss,mask_loss, regr_loss= criterion(output, mask_batch, regr_batch,1, size_average=False)
                 valid_loss += loss.data
                 valid_mask_loss += mask_loss.data
                 valid_regr_loss += regr_loss.data
             else :
-                loss,mask_loss, regr_loss = criterion(output, mask_batch, regr_batch,0.5, size_average=False)
+                loss,mask_loss, regr_loss = criterion(output, mask_batch, regr_batch,0.25, size_average=False)
                 valid_loss += loss.data
                 valid_mask_loss += mask_loss.data
                 valid_regr_loss += regr_loss.data 
@@ -110,63 +130,53 @@ def evaluate(epoch, history=None):
         history.loc[epoch, 'mask_loss'] = valid_mask_loss.cpu().numpy()
         history.loc[epoch, 'regr_loss'] = valid_regr_loss.cpu().numpy()
     
-    torch.save(model.state_dict(), 'model/resnet152/epoch{}.pth'.format(epoch+1))
+    torch.save(model.state_dict(), latest_model_path)
     
     print('Dev loss: {:.4f}'.format(valid_loss))
     
+    return valid_loss, model
+    
 if __name__ == '__main__':
-    SWITCH_LOSS_EPOCH = 5
-    
-    PATH = '/apl/kaggle/baidu_drive/data/'
  
-    train_images_dir = os.path.join(PATH, 'image/train/{}.jpg')
-    test_images_dir = os.path.join(PATH, 'image/test/{}.jpg')
+    train_images_dir = os.path.join(args.path, 'image/train/{}.jpg')
+    test_images_dir = os.path.join(args.path, 'image/test/{}.jpg')
     
-    train_csv = pd.read_csv(os.path.join(PATH, 'train.csv'))
-    test_csv = pd.read_csv(os.path.join(PATH, 'sample_submission.csv'))
+    train_csv = pd.read_csv(os.path.join(args.path, 'train.csv'))
+    test_csv = pd.read_csv(os.path.join(args.path, 'sample_submission.csv'))
     
-    camera_matrix = np.array([[2304.5479, 0,  1686.2379],
-                          [0, 2305.8757, 1354.9849],
-                          [0, 0, 1]], dtype=np.float32)
-    camera_matrix_inv = np.linalg.inv(camera_matrix)
-    
-    df_train, df_dev = train_test_split(train_csv, test_size=0.01, random_state=42)
+    df_train, df_dev = train_test_split(train_csv, test_size=0.05, random_state=42)
     df_test = test_csv
         
     train_dataset = dataset.CarDataset(df_train, train_images_dir)
     dev_dataset = dataset.CarDataset(df_dev, train_images_dir)
     test_dataset = dataset.CarDataset(df_test, test_images_dir)
-    
-    BATCH_SIZE = 1
+    print('train_data_num: {}'.format(len(train_dataset)))
+    print('dev_data_num: {}'.format(len(dev_dataset)))
     
     # Create data generators - they will produce batches
-    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
-    dev_loader = DataLoader(dataset=dev_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
-    
+    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch, shuffle=True, num_workers=0)
+    dev_loader = DataLoader(dataset=dev_dataset, batch_size=args.batch, shuffle=False, num_workers=0)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch, shuffle=False, num_workers=0)
     
     # Gets the GPU if there is one, otherwise the cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
-    n_epochs = 15 #6
-
     model = model.CentResnet(8).to(device)
     optimizer = optim.AdamW(model.parameters(), lr=0.001)
     #optimizer =  RAdam(model.parameters(), lr = 0.001)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=max(n_epochs, 10) * len(train_loader) // 3, gamma=0.1)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=max(args.epoch, 10) * len(train_loader) // 3, gamma=0.1)
     
-
-
     history = pd.DataFrame()
     
-    for epoch in range(n_epochs):
+    best_loss = 10000
+    
+    for epoch in range(args.epoch):
         print('epoch: {}'.format(epoch+1))
-        '''
-        torch.cuda.empty_cache()
-        gc.collect()
-        '''
         train(epoch, history)
-        evaluate(epoch, history)
+        valid_loss, model = evaluate(epoch, history)
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            torch.save(model.state_dict(), best_model_path)
         
-    history.to_csv('history_resnet152.csv')
+    history.to_csv(args.history_path)
